688856bcaeaaa78127f090304b3b7a8034b85306
Emine Zehra KÖSEOĞLU <ezehraksgl@gmail.com>
2024-12-19 15:46:51 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -5,5 +5,6 @@ int main() {
 
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
+printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
 
 }
