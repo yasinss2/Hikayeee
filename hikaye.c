a82b50cd890764e01ed2968dabcea8b28ecfb00f
burkira <147352689+burakgokmenn@users.noreply.github.com>
2024-12-19 15:43:05 +0300

    Hikaye başlangıcı hikaye.c
    Hikaye

--- hikaye.c
+++ hikaye.c
@@ -3,6 +3,7 @@
 
 int main() {
 
-printf("Soğuk bir kış akşamıydı\n");//Burak Gökmen hikaye başlangıcı
+printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
+printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
 
 }
