c58cc3774c834d9062a93b0c874894ffcf14fc4e
yasinss2 <yasint4@icloud.com>
2024-12-19 16:01:57 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -5,6 +5,7 @@ int main() {
 
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
 printf("Son çıkan efsanelerden sonra bu ormanın derinliklerinde güvende olmadıklarını biliyorlardı..");//Burak Gökmen hikaye başlangıcı
-printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..")
+printf("Kardeşlerden küçük olanı ormanın derinliklerinden gelen bir ses duyduğunu söyledi..");
+printf("Malzemeyi arap faik'ten alıyorduk karabükte ikinci yükleme yapılıyordu adanaya kadar da ben kullanıyordum kamyonu..");
 
 }
